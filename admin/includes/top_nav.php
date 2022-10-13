6c920779aacf4f68ead145c21984b6bc5e094d40
Konrad <konradbieniek@gmail.com>
2022-10-13 11:30:23 +0200

    database class

--- admin/includes/top_nav.php
+++ admin/includes/top_nav.php
@@ -4,7 +4,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin</a>
+                <a class="navbar-brand" href="../index.php">Home Page</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
