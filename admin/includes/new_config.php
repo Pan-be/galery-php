de8651b8fcc3d75cb0018408a0f217bae6400ea9
Konrad Bieniek <85822674+Pan-be@users.noreply.github.com>
2022-10-23 01:56:46 +0200

    Update new_config.php

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -2,9 +2,9 @@
 
 //Database connection constants
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','eu-cdbr-west-03.cleardb.net');
+define('DB_USER','b7496268ac801f');
+define('DB_PASS','a546b2de');
 define('DB_NAME','gallery_db');
 
 
