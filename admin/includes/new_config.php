fe198e7e4f4330218b1894b21c8725fc15e040de
Konrad Bieniek <85822674+Pan-be@users.noreply.github.com>
2022-10-23 13:51:57 +0200

    Update new_config.php

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -5,6 +5,6 @@
 define('DB_HOST','eu-cdbr-west-03.cleardb.net');
 define('DB_USER','b7496268ac801f');
 define('DB_PASS','a546b2de');
-define('DB_NAME','gallery_db');
+define('DB_NAME','heroku_083d91a3b3fc7fb');
 
 
